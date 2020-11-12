468dea0e828887189e9a40f2eb2c4c74307756fd
hamza.chaouch@esprit.tn <hamza.chaouch@esprit.tn>
2020-11-12 15:56:51 +0100

    first commit

---
+++ DependencyInjection/NotificationExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HamDev\NotifcationBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class NotificationExtension extends Extension{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Ressources/config'));
+        $loader->load('services.xml');
+    }
+}
